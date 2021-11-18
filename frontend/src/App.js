5b1ac505db4983b6e7fa0599cf2e02e70c26cfb2
PedroSalduna <pewemes@gmail.com>
2021-11-18 13:52:49 -0300

    tercer commit navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.css'
+// import logosecundario from './assets/logosecundario.png'
+// import {Nav, Navbar, NavDropdown} from 'react-bootstrap'
+import Home from './pags/Home.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hola mundo
-        </a>
-      </header>
+      <Home/>
     </div>
   );
 }
---
+++ frontend/src/pags/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navheader from '../componentes/Navheader';
+
+
+
+export default function Home () {
+
+    return (
+        <>
+        <Navheader/>
+        </>
+    )
+}
